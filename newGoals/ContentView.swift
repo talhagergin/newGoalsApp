bf81bf10df4348678e7d0e680a29b91b33588a49
Talha Gergin <talhagergin6@gmail.com>
2025-01-03 01:21:14 +0300

    Initial Commit

---
+++ newGoals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  newGoals
+//
+//  Created by Talha Gergin on 3.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ newGoals/newGoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  newGoalsApp.swift
+//  newGoals
+//
+//  Created by Talha Gergin on 3.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct newGoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
